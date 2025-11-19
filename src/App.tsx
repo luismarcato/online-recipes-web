80064ad8401a7c9428823253a6283090cad8e69e
Luis Eduardo <luis.marcato2000@gmail.com>
2025-11-18 21:07:15 -0300

    feat: add new recipes route

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router";
+import Index from "./pages/Index";
+import MainLayout from "./components/layout/MainLayout";
+
 function App() {
-  return <></>;
+  const queryClient = new QueryClient();
+
+  return (
+    <>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<MainLayout />}>
+              <Route index element={<Navigate to="/receitas" replace />} />
+              <Route path="receitas" element={<Index />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
+    </>
+  );
 }
 
 export default App;
